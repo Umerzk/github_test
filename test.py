d67583c50cee9ed26cf3ef68892603c6e287cb74
Umerzk <umerzk.pk@gmail.com>
2023-08-17 14:19:56 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-print ("pandora")
-print ('okay')
-print ('Elite')
+import gym
+
+env = gym.make('CartPole-v1')
+observation = env.reset()
+
+for _ in range(1000):
+    env.render()  # No need to specify the render mode
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+
+    if done:
+        observation = env.reset()
+
+env.close()

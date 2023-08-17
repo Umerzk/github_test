318c4da00c65f3a9ef7c462d43877b740fd301f1
Umerzk <umerzk.pk@gmail.com>
2023-08-17 15:12:12 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-import numpy as np
 import gym
 
 
 env = gym.make('CartPole-v1')
-env.reset()
+observation = env.reset()
 
 for _ in range(1000):
+    env.render()
     action = env.action_space.sample()
     env.step(action)
-    env.render()  # No need to specify the render mode
+
+env.close()

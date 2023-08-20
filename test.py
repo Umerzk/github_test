bdb6f6f3d2fd9a3c07acf441d2edc5fc1c91e27d
Umerzk <umerzk.pk@gmail.com>
2023-08-19 21:23:58 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-import gym
-
-env = gym.make("CartPole-v1")
-env.reset()
-
-for _ in range(1000):
-    env.render()
-    action = env.action_space.sample()
-    env.step(action)
-
-env.close()
+print("hi world")
